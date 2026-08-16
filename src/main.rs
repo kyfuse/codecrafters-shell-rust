1814094a678522c79da18e75d38661880a265c0c
Kyle Fu <43867185+kyfuse@users.noreply.github.com>
2026-08-16 01:32:39 -0700

    handle invalid commands

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    command = command.trim_end().to_string();
+    println!("{command}: command not found");
 }
