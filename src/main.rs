669ab2901148383a14964e96e4cb5db079fe6a5d
Kyle Fu <43867185+kyfuse@users.noreply.github.com>
2026-08-16 01:38:12 -0700

    implement a repl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
+use anyhow::Result;
 use std::io::{self, Write};
 
-fn main() {
+/// Reads the user's input.
+fn read() -> Result<String> {
     print!("$ ");
-    io::stdout().flush().unwrap();
+    io::stdout().flush()?;
 
     let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    command = command.trim_end().to_string();
-    println!("{command}: command not found");
+    io::stdin().read_line(&mut command)?;
+    Ok(command.trim_end().to_string())
+}
+
+/// Executes the given `command`.
+fn eval(command: &str) -> Result<()> {
+    anyhow::bail!("{command}: command not found")
+}
+
+fn main() -> Result<()> {
+    loop {
+        let command = read()?;
+        match eval(&command) {
+            Err(msg) => println!("{msg}"),
+            Ok(()) => todo!(),
+        };
+    }
 }
